3c82b6aff95567904cec341d7e4e6e9c1cf73204
elem14 <lemkee106@gmail.com>
2026-09-14 13:25:07 -0700

    added controllability check w/ controllability matrix C

---
+++ analysis/python/check_controllability.py
@@ -0,0 +1,69 @@
+# controllability matrix
+# C = [B, AB, A^2B, A^3B] 
+# VERIFY rank(C) = 4
+# if true then sys is completely controllable
+# rank should be 4, if not something has gone very wrong
+
+import numpy as np
+
+from cartpole_model import (
+    CartPoleParams,
+    linearized_state_space,
+)
+
+#temporary params again --- REPLACE AFTER MEASURING
+
+params = CartPoleParams(
+    cart_mass_kg=0.50,
+    pendulum_mass_kg=0.20,
+    pendulum_length_m=0.30,
+
+    cart_friction_n_s_m=0.0,
+    pivot_friction_n_m_s_rad=0.0,
+)
+
+A, B = linearized_state_space(
+    params
+)
+
+
+AB = A @ B
+A2B = A @ AB
+A3B = A @ A2B
+
+
+controllability_matrix = np.hstack(
+    [
+        B,
+        AB,
+        A2B,
+        A3B,
+    ]
+)
+
+rank = np.linalg.matrix_rank(
+    controllability_matrix
+)
+
+print("A =")
+print(A)
+
+print("\nB =")
+print(B)
+
+print("\nControllability matrix C =")
+print(controllability_matrix)
+
+print(
+    f"\nrank(C) = {rank}"
+)
+
+
+if rank == 4:
+    print(
+        "PASS: linearized system is controllable."
+    )
+else:
+    print(
+        "FAIL: linearized system is NOT fully controllable."
+    )
